f122130dc89c8ce6e05659a85e5bdaf551d6c7fc
thangbk2209 <thangbk2209@gmail.com>
2016-10-12 15:26:38 +0000

    ts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app=express();
 var http=require("http");
 var port=process.env.PORT||8080;
 app.get('/',function(req,res){
-    res.send('hello!!!');
+   // res.send('hello!!!');
     app.use(express.static("view"));
    // res.sendFile('view/index.html');
 })
