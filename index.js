3f0fb8a5d501a253c49f7b36f34c84d491a266b7
thangbk2209 <thangbk2209@gmail.com>
2016-10-13 13:50:57 +0000

    Fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app=express();
 var http=require("http");
 var port=process.env.PORT||8080;
 app.get('/',function(req,res){
-   // res.send('hello!!!');
-    app.use(express.static("view"));
+    res.send('hello!!!');
+   // app.use(express.static("view"));
    // res.sendFile('view/index.html');
 })
 app.get('/:dateString',function(req,res){
