59a66d1ce6d5588d066d423fa2f1f14a151c08a5
thangbk2209 <thangbk2209@gmail.com>
2016-10-13 14:10:35 +0000

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var port=process.env.PORT||8080;
 // app.set('views', path.join(__dirname, '../view'));
 // app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, "../view")));
+app.use(express.static(path.join(__dirname, "view")));
 // app.get('/', express.static(path.join(__dirname, "view")));
 // app.get('/',function(req,res){
     // res.send('hello!!!');
